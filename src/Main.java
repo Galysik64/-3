a5bedad0977c172e11cf49cc02066379b7f76a9b
Admin <galinapaskina61@gmail.com>
2024-10-05 13:56:32 +0400

    Home work 3

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+
+                System.out.println("Задание 1");
+                int one = 1400000;
+                byte two = 17;
+                short fhree = 10000;
+                long four = 100000;
+                float five = 0.7f;
+                double six = 5.5;
+                System.out.println("Значение переменной one с типом int равно " + one);
+                System.out.println("Значение переменной two с типом byte равно " + two);
+                System.out.println("Значение переменной fhree с типом short равно " + fhree);
+                System.out.println("Значение переменной four с типом long равно " + four);
+                System.out.println("Значение переменной five с типом float равно " + five);
+                System.out.println("Значение переменной six с типом double равно " + six);
+
+                System.out.println("Задание 2");
+                double oneOne = 27.12;
+                long twoTwo = 987678965549L;
+                float fhreeFhree = 2.786f;
+                int fourFour = 569;
+                short fiveFive =159;
+                long sixSix = 27897;
+                byte sevenSeven = 67;
+                System.out.println(oneOne);
+                System.out.println(twoTwo);
+                System.out.println(fhreeFhree);
+                System.out.println(fourFour);
+                System.out.println(fiveFive);
+                System.out.println(sixSix);
+                System.out.println(sevenSeven);
+
+                System.out.println("Задание 3");
+                byte lyda = 23;
+                byte anna = 27;
+                byte katy = 30;
+                int students = lyda + anna + katy;
+                int paper = 480;
+                int list = paper/students;
+                System.out.println("На каждого ученика рассчитано " + list + " листов бумаги.");
+
+
+
+
+
+    }
+}
